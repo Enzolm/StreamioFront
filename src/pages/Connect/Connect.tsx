56362b22ecf7f213cc093e5fac7a0dcace311d3d
Enzolm <legameurdesopr@gmail.com>
2024-10-24 09:49:23 +0200

    ...

--- src/pages/Connect/Connect.tsx
+++ src/pages/Connect/Connect.tsx
@@ -29,7 +29,9 @@ export default function Connect() {
   return (
     <>
       <form
-        className={`rounded-[37px] bg-main shadow-connect flex flex-col transition-all duration-700 items-center p-6 overflow-hidden h-[365px] w-[287px]`}
+        className={`rounded-[37px] bg-main shadow-connect flex flex-col transition-all duration-700 items-center p-6 overflow-hidden ${
+          isRegister ? "h-[365px] w-[287px]" : "h-[565px] w-[565px]"
+        }`}
         onSubmit={handleSubmit(onSubmit)}
       >
         <Link to="/">
@@ -39,13 +41,70 @@ export default function Connect() {
             alt="Logo"
           />
         </Link>
-        <m.div className="flex flex-col ">
-          <Input label="Entrez votre e-mail" type="email" />
-          <Input label="Entrez votre mot de passe" type="password" />
-          <Button label="Se connecter" />
-          <div className="w-full my-5 h-0.2 bg-gray-400 "></div>
-        </m.div>
 
+        {isRegister ? (
+          <m.div className="flex flex-col ">
+            <Input label="Entrez votre e-mail" type="email" />
+            <Input label="Entrez votre mot de passe" type="password" />
+            <Button label="Se connecter" />
+            <div className="w-full my-5 h-0.2 bg-gray-400 "></div>
+          </m.div>
+        ) : (
+          <m.div className="transition-all duration-700">
+            <div className="w-full grid-cols-2 grid gap-x-4">
+              <Input
+                label="Entrez votre prenom"
+                {...register("prenom")}
+                type="text"
+              />
+              <Input
+                label="Entrez votre nom"
+                {...register("nom")}
+                type="text"
+              />
+              <Input
+                label="Entrez votre ville"
+                {...register("ville")}
+                type="text"
+              />
+              <Input
+                label="Entrez votre code postal"
+                {...register("codepostal")}
+                type="text"
+              />
+              <Input
+                label="Entrez votre e-mail"
+                {...register("email")}
+                type="email"
+                className="col-span-2"
+              />
+              <Input
+                label="Entrez votre mot de passe"
+                {...register("motdepasse")}
+                type="password"
+                className="col-span-2"
+              />
+              <Input
+                label="Confirmez votre mot de passe"
+                name="motdepasseConfirm"
+                type="password"
+                className="col-span-2"
+              />
+              <Button
+                label="Se connecter"
+                type="submit"
+                className="col-span-2"
+              />
+            </div>
+            <div className="w-full my-5 h-0.2 bg-gray-400 "></div>
+          </m.div>
+        )}
+
+        <Register
+          isRegister={isRegister}
+          setIsRegister={setIsRegister}
+          className=""
+        />
         {errors.root && <p className="text-red-500">{errors.root.message}</p>}
       </form>
     </>
